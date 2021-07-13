cba812a04e9e89f75bf67e1dc7f704e20c4ffdac
siobhanlgorman <siobhanlgorman@gmail.com>
2021-07-13 11:39:13 +0000

    Add you die function to end game

--- run.py
+++ run.py
@@ -1,19 +1,19 @@
-# introduces game and greet user by name
+# introduces game and gets user's name
 def intro():
     print("Hello there\n")
     player_name = input("What's your name?\n")
     print(f"Hi {player_name}!")
 
-
+# function for when user dies and game ends
+def you_die(reason):
+    print(reason)
 
 # set scene
 
 def bedroom():
     print("You wake up in an unfamiliar room as dawn is breaking.\n Bit of a headache.\n Whose house is this again? \n Great party last night though.\n")
-    friend_name = input("Who did you come to the party with again?\n")
-    print(f"Oh yes ... {friend_name}")
     print("You stretch.\n Probably shouldn't have gone to the party what with that virus spreading and everything. \n Sure it'll never reach us here ")
-    print(f"Where the hell is {friend_name} anyway?\n")
+    print("Where the hell is Jay anyway? They crashed here too, didn't they\n")
     print("You hear some groaning from outside. You look out the window and see a couple of people from the party shuffling around the driveway near your car.\n Must've had too much to drink.\n")
     print("Better head off anyway.\n You pull on your clothes\n")
     print("There are two doors in the bedroom. One is on the left and one on the right.\n")
@@ -22,21 +22,20 @@ def bedroom():
     if door_choice == "left":
         print("You chose the door on the left")
         left_bathroom()
-        
     elif door_choice == "right":
         print("You chose the door on the right")
     else:
         print(input("Please enter left or right"))
 
 def left_bathroom():
-            print("You open the door. Just the bathroom. Before you go back out you notice a shape behind the shower curtain.\n")
-            choice = (input("Do you open the curtain? Yes or No\n").lower())
-            if choice == "yes":
-                print(f"Looks like you found friend_name…except it’s not friend_name any more....AGGGGHHHHHHH... Their skin is rotting and their eyes are white… they sink their teeth into your neck… YOU DIE")
-            elif choice == "no":
-                print("You picked no")
-            else:
-                print("Please choose yes or no")
+    print("You open the door. Just the bathroom. Before you go back out you notice a shape behind the shower curtain.\n")
+    choice = (input("Do you open the curtain? Yes or No\n").lower())
+    if choice == "yes":
+        you_die("Looks like you found Jay … except it’s not Jay any more....AGGGGHHHHHHH... Their skin is rotting and their eyes are white… they sink their teeth into your neck… YOU DIE")
+    elif choice == "no":
+        print("You picked no")
+    else:
+        print("Please choose yes or no")
     
 
 
